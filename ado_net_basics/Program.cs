0b88c4136881858f8a8f397c6fb9391074cf0166
yashwanth-dev <yashwanthp.developer@gmail.com>
2018-11-27 18:41:20 +0530

    Adonet basic implementation

---
+++ ado_net_basics/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+
+namespace ado_net_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+
+            var connectionString = @configuration["ConnectionString"];
+
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            ListExpectedtReceipts(connection);
+            connection.Close();
+
+            //## Test connection Open and close
+            // connection.Open();
+            // Console.WriteLine($"Connection State : {connection.State}");
+            // connection.Close();
+            // Console.WriteLine($"Connection State : {connection.State}");
+        }
+
+        private static void ListExpectedtReceipts(SqlConnection con)
+        {
+            var cmd = @"SELECT EXPECTEDRECEIPTKEY, EXPECTEDRECEIPTNUMBER, RECEIPTTYPE from dbo.ExpectedReceipt";
+            SqlCommand getExpectedReceiptsCmd = new SqlCommand(cmd);
+            getExpectedReceiptsCmd.Connection = con;
+
+            var reader = getExpectedReceiptsCmd.ExecuteReader();
+
+            int Expected_Receipt_key = reader.GetOrdinal("EXPECTEDRECEIPTKEY");
+            int Expected_Receipt_Number = reader.GetOrdinal("EXPECTEDRECEIPTNUMBER");
+            int Receipt_Type = reader.GetOrdinal("RECEIPTTYPE");
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"Expected Receipt Key: {reader.GetInt64(Expected_Receipt_key)}\nExpected Receipt Number: {reader.GetInt64(Expected_Receipt_Number)}\nReceipt Type: {reader.GetInt64(Receipt_Type)}\n");
+            }
+            reader.Close();
+
+
+        }
+    }
+}
---
+++ ado_net_basics/Readme.md
@@ -0,0 +1,17 @@
+https://docs.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code
+https://www.safaribooksonline.com/library/view/c-6-and/9781785285691/ch08s02.html
+Create console app using the following command:
+dotnet new console
+
+To run the app use the following command:
+dotnet run or Press F5 in VS code to run in debug mode
+
+Troubleshooting:
+Replace configuration --> program settings with app_name and framework_versionnumber
+Add tasks.json file when prompted
+
+nuget packages added:
+ado_net_basics.csproj --> Under <ItemGroup>
+
+// TODO:
+Implement unit of work pattern where connection opening and closing is taken care of
