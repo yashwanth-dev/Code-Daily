d0bce6c40b90459658536b70500b6c50ef7d2ac6
yashwanth-dev <yashwanth.github@gmail.com>
2018-12-08 16:47:43 +0530

    Added subscription and handled actions in state.

--- react_prototypes/Redux/Readme.md
+++ react_prototypes/Redux/Readme.md
@@ -5,3 +5,7 @@ Actions
     Payload
 Reducer
     State and Action Management
+
+Redux core concepts: https://redux.js.org/introduction/coreconcepts
+Redux three principles: https://redux.js.org/introduction/threeprinciples
+
--- react_prototypes/Redux/redux-basics.js
+++ react_prototypes/Redux/redux-basics.js
@@ -8,6 +8,19 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === "INC_Counter") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    } else if (action.type === "Add_Counter") {
+        return {
+            ...state,
+            counter: state.counter + 10
+        }
+    }
+
+
     return state;
 }
 
@@ -15,6 +28,13 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-// Dispatching Action
-
 // Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+})
+
+// Dispatching Action
+store.dispatch({ type: "INC_Counter" });
+store.dispatch({ type: "Add_Counter", value: 10 });
+console.log(store.getState());
+
