5af72962a23dfd33fd7a1f864688b7c673345a74
yashwanth-dev <yashwanthp.developer@gmail.com>
2019-01-03 17:11:37 +0530

    parallel.invoke

---
+++ c_sharp_parallel_async_programming/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace c_sharp_parallel_async_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() => DoWork(), () => DoSomeOtherWork());
+        }
+
+        public static void DoWork()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Doing work {i}");
+            }
+        }
+
+        public static void DoSomeOtherWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Doing Some other work {i}");
+            }
+        }
+    }
+}
